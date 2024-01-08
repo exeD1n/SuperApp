56e9e2fa107b5eff7d24093c1d9920be66783562
Daniil Lutaev <daniillutaev1@gmail.com>
2024-01-08 09:41:40 +0400

    dfg

--- System/app.py
+++ System/app.py
@@ -401,11 +401,27 @@ class ProcessesWindow:
         self.refresh_button = tk.Button(self.processes_window, text="Обновить", command=self.update_processes_data)
         self.refresh_button.pack(pady=10)
 
+        self.save_button = tk.Button(self.processes_window, text="Сохранить в файл", command=self.save_to_file)
+        self.save_button.pack(pady=10)
+
         # Кнопка для возвращения в главное меню
         self.return_button = tk.Button(self.processes_window, text="Вернуться в главное меню", command=self.return_to_main_menu)
         self.return_button.pack(pady=10)
 
         self.update_processes_data()
+
+    def save_to_file(self):
+        file_path = "MyDocuments/processes_info.txt"
+        with open(file_path, 'w') as file:
+            for process in psutil.process_iter(['pid', 'name', 'create_time']):
+                if process.info['name'] != 'python.exe' and process.info['pid'] != os.getpid():
+                    process_name = process.info['name']
+                    create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(process.info['create_time']))
+                    item_text = f"{process_name} (PID: {process.info['pid']}, Запущен: {create_time})"
+                    file.write(item_text + '\n')
+
+        messagebox.showinfo("Успех", f"Информация о процессах сохранена в файл: {file_path}")
+
 
     def update_processes_data(self):
         self.process_listbox.delete(0, tk.END)
